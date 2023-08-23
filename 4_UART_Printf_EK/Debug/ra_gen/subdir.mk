3631ef817520b663d20a3a75950b743dce95e1fe
mehmettartan <91870337+mehmettartan@users.noreply.github.com>
2023-08-23 16:47:48 +0300

    EK-RA2E1_UART_Print

---
+++ 4_UART_Printf_EK/Debug/ra_gen/subdir.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../ra_gen/common_data.c \
+../ra_gen/hal_data.c \
+../ra_gen/main.c \
+../ra_gen/pin_data.c \
+../ra_gen/vector_data.c 
+
+C_DEPS += \
+./ra_gen/common_data.d \
+./ra_gen/hal_data.d \
+./ra_gen/main.d \
+./ra_gen/pin_data.d \
+./ra_gen/vector_data.d 
+
+OBJS += \
+./ra_gen/common_data.o \
+./ra_gen/hal_data.o \
+./ra_gen/main.o \
+./ra_gen/pin_data.o \
+./ra_gen/vector_data.o 
+
+SREC += \
+4_UART_Printf_EK.srec 
+
+MAP += \
+4_UART_Printf_EK.map 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+ra_gen/%.o: ../ra_gen/%.c
+	$(file > $@.in,-mcpu=cortex-m23 -mthumb -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -gdwarf-4 -D_RA_CORE=CM23 -D_RENESAS_RA_ -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/src" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/ra/fsp/inc" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/ra/fsp/inc/api" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/ra/fsp/inc/instances" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/ra/arm/CMSIS_5/CMSIS/Core/Include" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/ra_gen" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/ra_cfg/fsp_cfg/bsp" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/4_UART_Printf_EK/ra_cfg/fsp_cfg" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
+
