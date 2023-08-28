9148211cb87cd0c6d968e5773418c046841e1129
mehmettartan <91870337+mehmettartan@users.noreply.github.com>
2023-08-28 16:14:35 +0300

    EK-RA2E1_RTT(real_terminal transfer)

---
+++ 6_RTT_EK/Debug/ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.d
@@ -0,0 +1,131 @@
+ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.o: \
+ ../ra/fsp/src/bsp/cmsis/Device/RENESAS/Source/system.c \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/api/bsp_api.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/fsp_common_api.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/fsp_version.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_cfg.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_gen/bsp_clock_cfg.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_mcu_info.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_elc.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_icu.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_feature.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/board_cfg.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\board\ra2e1_ek\board.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\board\ra2e1_ek\board_init.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\board\ra2e1_ek\board_leds.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_gen/vector_data.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA2E1A9.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm23.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_common.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\inc\fsp_common_api.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_irq.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_io.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_clocks.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_security.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\inc\fsp_features.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\inc\fsp_common_api.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_delay.h \
+ c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/api/bsp_api.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/fsp_common_api.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/fsp_version.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_cfg.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_gen/bsp_clock_cfg.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_mcu_family_cfg.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_pn_cfg.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/bsp_mcu_device_cfg.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_mcu_info.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_elc.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_icu.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\ra2e1\bsp_feature.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp/board_cfg.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\board\ra2e1_ek\board.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\board\ra2e1_ek\board_init.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\board\ra2e1_ek\board_leds.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_arm_exceptions.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_gen/vector_data.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\renesas.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\R7FA2E1A9.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/core_cm23.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_version.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h:
+
+C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include/mpu_armv8.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\cmsis\device\renesas\include\system.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_common.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\inc\fsp_common_api.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_register_protection.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_irq.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_io.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_group_irq.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_clocks.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_module_stop.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_security.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\inc\fsp_features.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\inc\fsp_common_api.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_delay.h:
+
+c:\users\mehmet.tartan.alpmerkez\e2_studio\workspace\fsp230\ra2e1\6_rtt\ra\fsp\src\bsp\mcu\all\bsp_mcu_api.h:
---
+++ 6_RTT_EK/Debug/src/subdir.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/hal_entry.c 
+
+C_DEPS += \
+./src/hal_entry.d 
+
+OBJS += \
+./src/hal_entry.o 
+
+SREC += \
+6_RTT.srec 
+
+MAP += \
+6_RTT.map 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	$(file > $@.in,-mcpu=cortex-m23 -mthumb -O2 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wunused -Wuninitialized -Wall -Wextra -Wmissing-declarations -Wconversion -Wpointer-arith -Wshadow -Wlogical-op -Waggregate-return -Wfloat-equal  -g -gdwarf-4 -D_RA_CORE=CM23 -D_RENESAS_RA_ -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/src" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/api" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/fsp/inc/instances" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra/arm/CMSIS_5/CMSIS/Core/Include" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_gen" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg/bsp" -I"C:/Users/mehmet.tartan.ALPMERKEZ/e2_studio/workspace/fsp230/RA2E1/6_RTT/ra_cfg/fsp_cfg" -std=c99 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" -x c "$<")
+	@echo Building file: $< && arm-none-eabi-gcc @"$@.in"
+
